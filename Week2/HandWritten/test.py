ad4e98dae340077c1da8bb70252c428e87f83d5d
Lu Phone Maw <luphonemaw09@gmail.com>
2024-07-10 01:30:07 +0700

    Delete Week2 directory

--- Week2/HandWritten/test.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-from keras.models import load_model
-
-
-# Load the model
-model = load_model('/Users/richard/Desktop/DeepLearningABAC/Week2/HandWritten/mnist.h5')
-
-# Add this code after loading the model
-print(model.summary())
